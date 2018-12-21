496048858ce903a8da17b263211e7ec11ea90d49
Nke Yapo <yaposebastien@gmail.com>
2018-12-20 21:58:58 -0500

    Scaffold the movie model

---
+++ CSC496/ProjectOne/RazorMovieApp/Pages/Movies/Create.cshtml.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using RazorMovieApp.Models;
+
+namespace RazorMovieApp.Pages.Movies
+{
+    public class CreateModel : PageModel
+    {
+        private readonly RazorMovieApp.Models.RazorMovieAppContext _context;
+
+        public CreateModel(RazorMovieApp.Models.RazorMovieAppContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Movie Movie { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Movie.Add(Movie);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
