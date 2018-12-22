fdfd38248fd85d0bf20bee414c560152364db400
Nke Yapo <yaposebastien@gmail.com>
2018-12-21 20:00:30 -0500

    Adding search module for Razor Model

--- CSC496/ProjectOne/RazorMovieApp/Pages/Movies/Index.cshtml.cs
+++ CSC496/ProjectOne/RazorMovieApp/Pages/Movies/Index.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using RazorMovieApp.Models;
 
@@ -19,10 +20,33 @@ namespace RazorMovieApp.Pages.Movies
         }
 
         public IList<Movie> Movie { get;set; }
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
+        
+        public SelectList Genres { get; set; }
+        [BindProperty(SupportsGet = true)]
+        public string MovieGenre { get; set; }
+
 
         public async Task OnGetAsync()
         {
-            Movie = await _context.Movie.ToListAsync();
+            var movies = from m in _context.Movie select m;
+            //Uasage of LINQ to get list of Genres
+            IQueryable<string> genreQuery = from m in _context.Movie orderby m.Genre select m.Genre;
+
+            if (!String.IsNullOrEmpty(SearchString)) {
+                movies = movies.Where(s => s.Title.Contains(SearchString));
+            }
+
+            //Updating OnGetAsync to trigger search by Genre
+            if (!String.IsNullOrEmpty(MovieGenre)) {
+                movies = movies.Where(x => x.Genre == MovieGenre);
+
+            }
+
+            //Query for Searching by Genre
+            Genres = new SelectList(await genreQuery.Distinct().ToListAsync());
+            Movie = await movies.ToListAsync();
         }
     }
 }
