22360995280030c83f6e29fa8682194b312f0c38
Nke Yapo <yaposebastien@gmail.com>
2018-12-21 20:18:32 -0500

    Search function added

--- CSC496/ProjectOne/RazorMovieApp/Migrations/20181222011525_AddData2.Designer.cs
+++ CSC496/ProjectOne/RazorMovieApp/Migrations/20181222011525_AddData2.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorMovieApp.Models;
 
 namespace RazorMovieApp.Migrations
 {
     [DbContext(typeof(RazorMovieAppContext))]
-    partial class RazorMovieAppContextModelSnapshot : ModelSnapshot
+    [Migration("20181222011525_AddData2")]
+    partial class AddData2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- CSC496/ProjectOne/RazorMovieApp/Models/Movie.cs
+++ CSC496/ProjectOne/RazorMovieApp/Models/Movie.cs
@@ -15,5 +15,7 @@ namespace RazorMovieApp.Models
         public string Genre { get; set; }
 		[Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+
+		public string Rating { get; set;}
     }
 }
