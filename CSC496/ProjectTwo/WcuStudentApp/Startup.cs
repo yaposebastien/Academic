4d7530e833fb5497a461c777aab8edef18376eb7
Nke Yapo <yaposebastien@gmail.com>
2018-12-26 00:45:05 -0500

    Registered the database context into Startup.cs file

--- CSC496/ProjectTwo/WcuStudentApp/Startup.cs
+++ CSC496/ProjectTwo/WcuStudentApp/Startup.cs
@@ -33,7 +33,8 @@ namespace WcuStudentApp
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-
+            //Registering Database Context
+            services.AddDbContext<MvcStudentContext>(options => options.UseSqlite(Configuration.GetConnectionString("StudentContext")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
