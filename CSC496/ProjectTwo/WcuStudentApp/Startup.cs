88edcc83b8fec17a3cef62c2a71e166f54a1b877
Nke Yapo <yaposebastien@gmail.com>
2018-12-26 00:41:31 -0500

    Registered the database context into Startup.cs file

--- CSC496/ProjectTwo/WcuStudentApp/Startup.cs
+++ CSC496/ProjectTwo/WcuStudentApp/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using WcuStudentApp.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace WcuStudentApp
 {
