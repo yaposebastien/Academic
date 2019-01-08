7d0dbfe738358e6a5c8678390b72ff93e7760b38
Nke Yapo <yaposebastien@gmail.com>
2019-01-08 10:37:50 -0500

    Adding JQuery functionality

--- CSC496/ProjectThree/TodoApi/Startup.cs
+++ CSC496/ProjectThree/TodoApi/Startup.cs
@@ -49,6 +49,10 @@ namespace TodoApi
                 app.UseHsts();
             }
 
+            
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+
             app.UseHttpsRedirection();
             app.UseMvc();
         }
